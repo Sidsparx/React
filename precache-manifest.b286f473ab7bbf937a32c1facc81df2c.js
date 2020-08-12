92b53a766d1c60cbe4e04a401110946747646856
Sam <Sam Jane>
2020-08-12 10:54:22 +0530

    Updates

---
+++ precache-manifest.b286f473ab7bbf937a32c1facc81df2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf20b454e7341231354fb319e115795",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "981295a8558c83efd708",
+    "url": "/React/static/css/2.62ff64f5.chunk.css"
+  },
+  {
+    "revision": "981295a8558c83efd708",
+    "url": "/React/static/js/2.b210410d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React/static/js/2.b210410d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7bd39c89fae0b8fae58",
+    "url": "/React/static/js/main.139b6825.chunk.js"
+  },
+  {
+    "revision": "64f3dca2d6ac8fc5be7e",
+    "url": "/React/static/js/runtime-main.ab40f63e.js"
+  }
+]);
